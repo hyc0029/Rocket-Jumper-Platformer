69674a29c22efa0355a052b94498d187f620c2b9
hyc0029 <55817300+hyc0029@users.noreply.github.com>
2021-09-23 09:19:06 -0700

    character setup

--- Assets/Scripts/RockentControl.cs
+++ Assets/Scripts/RockentControl.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class RockentControl : MonoBehaviour
 {
 
-    [SerializeField] private Transform shoulder;
+    [SerializeField] private Transform rightShoulder;
+    [SerializeField] private Transform leftShoulder;
+    private float rightShoulderInitialAngle;
+    private float leftShoulderInitialAngle;
+    private float leftShoulderAngleDiff;
+    [SerializeField] private Transform rocket;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rightShoulderInitialAngle = rightShoulder.eulerAngles.z;
+        leftShoulderInitialAngle = leftShoulder.eulerAngles.z;
+        leftShoulderAngleDiff = rightShoulderInitialAngle - leftShoulderInitialAngle;
     }
 
     // Update is called once per frame
@@ -23,13 +30,25 @@ public class RockentControl : MonoBehaviour
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector2 vectorToTarget = mousePos - (Vector2)shoulder.position;
+        Vector2 vectorToTarget = mousePos - (Vector2)rocket.position;
 
-        float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) + 90;
+        float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) + rightShoulderInitialAngle;
 
         Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        shoulder.rotation = Quaternion.Slerp(shoulder.rotation, rot, 1);
+        rightShoulder.rotation = Quaternion.Slerp(rightShoulder.rotation, rot, 1);
+
+        float leftAngle = angle - leftShoulderAngleDiff;
+
+        Quaternion lRot = Quaternion.AngleAxis(leftAngle, Vector3.forward);
+
+        leftShoulder.rotation = Quaternion.Slerp(leftShoulder.rotation, lRot, 1);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(rocket.position, rocket.right * 100);
     }
 
 }
