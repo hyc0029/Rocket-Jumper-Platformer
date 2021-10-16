542f4a54da235828b0ae7ad32f704dab0fb7ce56
hyc0029 <55817300+hyc0029@users.noreply.github.com>
2021-10-15 21:33:46 -0700

    rocket emission and smooth camera

--- Assets/1.RocketJumperFiles/Scripts/Player/CameraController.cs
+++ Assets/1.RocketJumperFiles/Scripts/Player/CameraController.cs
@@ -41,7 +41,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (player.myCol.enabled)
         {
